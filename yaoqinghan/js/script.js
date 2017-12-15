fe56e5377dd900a1535b998e0c1958f76c887d0e
wangzhisen <1242790576@qq.com>
2017-12-15 16:59:24 +0800

    邀请函

---
+++ yaoqinghan/js/script.js
@@ -0,0 +1,55 @@
+var swiper = new Swiper('.swiper-container', {
+    //Slides的滑动方向，可设置水平(horizontal)或垂直(vertical)。
+    direction: 'vertical',
+    //分页器
+    pagination: {
+        el: '.swiper-pagination',
+        //让分页器支持点击
+        clickable: true,
+    },
+
+    // 前进按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+    },
+
+    //动画效果
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+});
+
+
+var mp3=document.querySelector("#mp3");
+var music=document.querySelector("#music");
+
+music.addEventListener("click",function(){
+    if(mp3.paused){
+        mp3.play();
+    }else {
+        mp3.pause();
+    }
+})
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
